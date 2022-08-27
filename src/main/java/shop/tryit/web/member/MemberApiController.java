887e26d53b5c95c63b5bfa6c666c352bc3924341
Kimjunghyunn <wjdgus6846@gmail.com>
2022-08-27 15:38:19 +0900

    feat: 회원 수정 rest controller 추가

--- src/main/java/shop/tryit/web/member/MemberApiController.java
+++ src/main/java/shop/tryit/web/member/MemberApiController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -58,6 +59,40 @@ public class MemberApiController {
 
         // 성공 시 201
         return ResponseEntity.created(location).body("회원 등록 성공");
+    }
+
+    /**
+     * 회원 정보 수정
+     */
+    @PutMapping("/update")
+    public ResponseEntity editMember(@Valid @RequestBody @ModelAttribute("memberForm") MemberFormDto memberForm,
+                                     BindingResult bindingResult) {
+        log.info("회원 수정으로 이동");
+        if (!memberForm.getPassword1().equals(memberForm.getPassword2())) {
+            bindingResult.rejectValue("password2", "passwordInCorrect",
+                    "비밀번호가 일치하지 않습니다.");
+        }
+
+        // 검증 실패시 400
+        if (bindingResult.hasErrors()) {
+            log.info("member controller post");
+            StringBuilder sb = new StringBuilder();
+
+            for (FieldError error : bindingResult.getFieldErrors())
+                sb.append(error.getDefaultMessage());
+
+            return new ResponseEntity<>(sb.toString(), HttpStatus.BAD_REQUEST);
+        }
+
+        memberFacade.update(memberForm);
+        URI location = UriComponentsBuilder.newInstance()
+                .scheme("http")
+                .path("tryeat.shop/members/update")
+                .build()
+                .toUri();
+
+        // 성공 시 201
+        return ResponseEntity.created(location).body("회원 수정 성공");
     }
 
 }
